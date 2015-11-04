390b35b7429f481164381f0e4e3b046ff043b8da
Eric P. Katz <ericpkatz@gmail.com>
2015-11-03 20:45:08 -0500

    add google analytics

--- browser/js/cart/cart_factory.js
+++ browser/js/cart/cart_factory.js
@@ -33,6 +33,24 @@ app.factory('CartFactory', function($http){
         return cart();
       })
       .then(function(){
+        //record the transaction
+        ga('require', 'ecommerce');
+        ga('ecommerce:addTransaction', {
+          'id': '1234',                     // Transaction ID. Required.
+          'affiliation': 'Acme Clothing',   // Affiliation or store name.
+          'revenue': '11.99',               // Grand Total.
+          'shipping': '5',                  // Shipping.
+          'tax': '1.29'                     // Tax.
+        });
+        ga('ecommerce:addItem', {
+          'id': '1234',                     // Transaction ID. Required.
+          'name': 'Fluffy Pink Bunnies',    // Product name. Required.
+          'sku': 'DD23444',                 // SKU/code.
+          'category': 'Party Toys',         // Category or variation.
+          'price': '11.99',                 // Unit price.
+          'quantity': '1'                   // Quantity.
+        });
+        ga('ecommerce:send');
         return order;
       });
   }
